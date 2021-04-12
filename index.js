85000447db4874feb82d568b6f1298dc82d934ce
Iryna Stein <isophiestein@gmail.com>
2021-04-11 21:56:07 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.querySelector ('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader); 
+document.getElementsByTagName("h1")[0].setAttribute("id", "victory");
+
+const headerText = document.getElementById('victory');
+headerText.innerHTML = "YOUR-NAME is the champion"
