72dedb6e21f9d73495d8e0c336704ae504686b15
Iryna Stein <isophiestein@gmail.com>
2021-05-04 13:01:02 -0600

    Updated old code

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // Write your code here!
-const main = document.querySelector ('main');
-main.remove();
+document.querySelector('main').remove()
 
 const newHeader = document.createElement('h1');
-document.body.appendChild(newHeader); 
-document.getElementsByTagName("h1")[0].setAttribute("id", "victory");
+document.body.appendChild(newHeader);
 
-const headerText = document.getElementById('victory');
-headerText.innerHTML = "YOUR-NAME is the champion"
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML = "Iryna is the champion";
+
+// Below is old code from pre-work phase of the course:
+// const main = document.querySelector ('main');
+// main.remove();
+
+// const newHeader = document.createElement('h1');
+// document.body.appendChild(newHeader); 
+// document.getElementsByTagName("h1")[0].setAttribute("id", "victory");
+
+// const headerText = document.getElementById('victory');
+// headerText.innerHTML = "YOUR-NAME is the champion"
